d8e81299481c743b074d52151597cac07073a2a4
Jose Padron <padroncjosel@gmail.com>
2022-08-05 12:02:11 -0400

    refactor: modificaciones en readme y strategy validate user

---
+++ src/modules/users/__test__/users.controller.spec.ts
@@ -0,0 +1,5 @@
+describe('UsersController', () => {
+  it('hola', () => {
+    expect('hola').toEqual('hola');
+  });
+});
---
+++ src/modules/users/__test__/users.service.spec.ts
@@ -0,0 +1,5 @@
+describe('UsersServices', () => {
+  it('hola', () => {
+    expect('hola').toEqual('hola');
+  });
+});
